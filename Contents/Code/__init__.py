98f83386573a7418ff7f3970f9dea35917999bbe
sander1 <sander@plexapp.com>
2011-10-19 17:30:17 +0200

    Use same indentation everywhere; add iOS and Android to PlexClientPlatforms; remove unused string file; optimize PNGs

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,11 +3,11 @@ import re
 TO_PLUGIN_PREFIX   = "/video/theonion"
 TO_BASE_URL        = "http://www.theonion.com"
 
-CACHE_INTERVAL       = 1000 # HTTP cache interval in seconds
-MAX_RESULTS          = "100"
+CACHE_INTERVAL     = 1000 # HTTP cache interval in seconds
+MAX_RESULTS        = "100"
 
-MAIN_PAGE            = "http://www.theonion.com/content/video"
-TO_AJAX              = "http://www.theonion.com/ajax/onn_playlist/%s/%s" #% show name %pagenum
+MAIN_PAGE          = "http://www.theonion.com/content/video"
+TO_AJAX            = "http://www.theonion.com/ajax/onn_playlist/%s/%s" #% show name %pagenum
 
 NAME = "The Onion"
 ART = "art-default.jpg"
@@ -31,72 +31,73 @@ def Start():
 
 ####################################################################################################
 def MainMenu():
-   oc = ObjectContainer()
-   
-   oc.add(DirectoryObject(key = Callback(Shows), title = "Shows"))
+  oc = ObjectContainer()
+  oc.add(DirectoryObject(key = Callback(Shows), title = "Shows"))
 
-   page = HTML.ElementFromURL(MAIN_PAGE, cacheTime = CACHE_INTERVAL)
-   categories = page.xpath('//div[@id="side_recirc"]/div')
-   for category in categories:
-     id = category.get("rel")
-     title = category.xpath("a")[0].text
+  page = HTML.ElementFromURL(MAIN_PAGE, cacheTime = CACHE_INTERVAL)
+  categories = page.xpath('//div[@id="side_recirc"]/div')
 
-     oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title), title = title))
-   oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.theonion", title = "Search...", prompt = "Search for clips", thumb = R(SEARCH)))
-   return oc
+  for category in categories:
+    id = category.get("rel")
+    title = category.xpath("a")[0].text
+    oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title), title = title))
+
+  oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.theonion", title = "Search...", prompt = "Search for clips", thumb = R(SEARCH)))
+  return oc
 
 ####################################################################################################
 def Shows():
-   oc = ObjectContainer(title2 = "Shows")  
+  oc = ObjectContainer(title2 = "Shows")
 
-   page = HTML.ElementFromURL(MAIN_PAGE, cacheTime = CACHE_INTERVAL)
-   shows = page.xpath('//ul[@id="categories"]//li')
-   for show in shows:
-     if show.get("class") != "label":
-       title = show.xpath("a")[0].text_content()
-       id = show.get("rel")
+  page = HTML.ElementFromURL(MAIN_PAGE, cacheTime = CACHE_INTERVAL)
+  shows = page.xpath('//ul[@id="categories"]//li')
 
-       oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title), title = title))
-   return oc
+  for show in shows:
+    if show.get("class") != "label":
+      title = show.xpath("a")[0].text_content()
+      id = show.get("rel")
+      oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title), title = title))
+
+  return oc
 
 ####################################################################################################
 def PopulateFromHTML(show_id, show_title = '', replace_parent = False,  page = 1):
   oc = ObjectContainer(view_group = "InfoList", title2 = str(show_title) + ' - Page ' + str(page), replace_parent = replace_parent)
-    
+
   if page > 1 :
     oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title, page = page - 1, replace_parent = True), title = 'Previous Page ...'))
-  
+
   page = HTML.ElementFromURL(TO_AJAX % (show_id, page))
+
   for element in page.xpath("//li"):
-      
     url = TO_BASE_URL + element.xpath('.//a')[0].get('href')
     if url.startswith('/video/') == False:
       continue
-      
+
     title = element.xpath('.//p[@class="title"]//text()')[0]
     summary = element.xpath('.//p[@class="teaser"]')[0].text
     thumb = element.xpath('.//img')[0].get('src')
-      
+
     info = element.xpath('//li//p[@class="info"]/text()')[0].strip()
     info_dict = re.match("\((?P<mins>[0-9]+):(?P<secs>[0-9]+)\)", info).groupdict()
     mins = int(info_dict['mins'])
     secs = int(info_dict['secs'])
     duration = ((mins * 60) + secs) * 1000
-      
+
     oc.add(VideoClipObject(
       url = url,
       title = title,
       summary = summary,
       thumb = Function(GetThumb,url=thumb),
       duration = duration))
-    
+
   try: 
     resp = HTTP.Request(TO_AJAX % (show_id, int(page + 1))).content
     oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title, page = page + 1, replace_parent = replace_parent), title = 'Next Page ...'))
     dir.Append(Function(DirectoryItem(populateFromHTML, title = 'Next Page ...'),show_id = show_id, show_title = show_title, page = page+1, replaceParent = True))
   except:
     pass
-    
+
   return oc
 
 ####################################################################################################
