1ef37e0fd494918254888dd5e816768c5535c67e
sander1 <sander@plexapp.com>
2013-09-30 02:49:45 +0200

    Rewrite/update, use hosted resources.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,117 +1,66 @@
-TO_PLUGIN_PREFIX   = "/video/theonion"
-TO_BASE_URL        = "http://www.theonion.com"
-
-CACHE_INTERVAL     = 1000 # HTTP cache interval in seconds
-MAX_RESULTS        = "100"
-
-MAIN_PAGE          = "http://www.theonion.com/content/video"
-TO_AJAX            = "http://www.theonion.com/ajax/onn_playlist/%s/%s" #% show name %pagenum
-
-NAME = "The Onion"
-ART = "art-default.jpg"
-ICON = "icon-default.jpg"
-SEARCH = "icon-search.png"
+NAME = 'The Onion'
+BASE_URL = 'http://www.theonion.com'
+MAIN_PAGE = 'http://www.theonion.com/video'
 
 RE_DURATION = Regex("\((?P<mins>[0-9]+):(?P<secs>[0-9]+)\)")
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(TO_PLUGIN_PREFIX, MainMenu, NAME, ICON, ART)
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")  
-  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-  ObjectContainer.art = R(ART)
+  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
+
   ObjectContainer.title1 = NAME
-  ObjectContainer.view_group = "List"
-
-  DirectoryObject.thumb = R(ICON)
-  DirectoryObject.art = R(ART)
-  VideoClipObject.thumb = R(ICON)
-  VideoClipObject.art = R(ART)
+  ObjectContainer.view_group = 'List'
+  HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
+@handler('/video/theonion', NAME)
 def MainMenu():
+
   oc = ObjectContainer()
-  oc.add(DirectoryObject(key = Callback(Shows), title = "Shows"))
+  html = HTML.ElementFromURL(MAIN_PAGE)
 
-  page = HTML.ElementFromURL(MAIN_PAGE, cacheTime = CACHE_INTERVAL)
-  categories = page.xpath('//div[@id="side_recirc"]/div')
+  for show in html.xpath('//img[@title="The Onion Video"]/../../..//li//a'):
+    title = show.xpath('./text()')[0]
+    url = show.xpath('./@href')[0]
 
-  for category in categories:
-    id = category.get("rel")
-    title = category.xpath("a")[0].text
-    oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title), title = title))
-
-  oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.theonion", title = "Search...", prompt = "Search for clips", thumb = R(SEARCH)))
-  return oc
-
-####################################################################################################
-def Shows():
-  oc = ObjectContainer(title2 = "Shows")
-
-  page = HTML.ElementFromURL(MAIN_PAGE, cacheTime = CACHE_INTERVAL)
-  shows = page.xpath('//ul[@id="categories"]//li')
-
-  for show in shows:
-    if show.get("rel") not in ["", "featured", "mostrecent"]:
-      title = ' '.join(show.xpath("./a//text()"))
-      id = show.get("rel")
-      oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title), title = title))
+    if title not in ('Promos'):
+      oc.add(DirectoryObject(
+        key = Callback(Episodes, show_url=url, show_title=title),
+        title = title
+      ))
 
   return oc
 
 ####################################################################################################
-def PopulateFromHTML(show_id, show_title = '', replace_parent = False,  page = 1):
-  oc = ObjectContainer(view_group = "InfoList", title2 = str(show_title) + ' - Page ' + str(page), replace_parent = replace_parent)
+@route('/video/theonion/episodes', page=int)
+def Episodes(show_url, show_title, page=1):
 
-  if page > 1 :
-    oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title, page = page - 1, replace_parent = True), title = 'Previous Page ...'))
+  oc = ObjectContainer(title2=show_title)
+  html = HTML.ElementFromURL('%s%s?page=%d' % (BASE_URL, show_url, page))
 
-  page = HTML.ElementFromURL(TO_AJAX % (show_id, page))
+  for episode in html.xpath('//div[@class="episodes"]/article'):
+    url = episode.xpath('.//h1/a/@href')[0]
 
-  for element in page.xpath("//li"):
-    url = element.xpath('.//a')[0].get('href')
+    if not url.startswith('http://'):
+      url = '%s%s' % (BASE_URL, url)
 
-    if url.startswith('/video/') == False:
-      continue
-    else:
-      url = TO_BASE_URL + url
-
-    title = element.xpath('.//p[@class="title"]//text()')[0]
-    summary = element.xpath('.//p[@class="teaser"]')[0].text
-    thumb = element.xpath('.//img')[0].get('src')
-
-    info = element.xpath('.//p[@class="info"]/text()')[0].strip()
-    info_dict = RE_DURATION.match(info).groupdict()
-    mins = int(info_dict['mins'])
-    secs = int(info_dict['secs'])
-    duration = ((mins * 60) + secs) * 1000
+    title = episode.xpath('.//h1/a/text()')[0]
+    thumb = episode.xpath('.//img/@data-src')[0].replace('/260.jpg', '/640.jpg').split('?')[0]
+    duration = episode.xpath('.//span[@class="duration"]/text()')[0]
 
     oc.add(VideoClipObject(
       url = url,
       title = title,
-      summary = summary,
-      thumb = Function(GetThumb,url=thumb),
-      duration = duration))
+      thumb = Resource.ContentsOfURLWithFallback(thumb),
+      duration = Datetime.MillisecondsFromString(duration)
+    ))
 
-  try: 
-    resp = HTTP.Request(TO_AJAX % (show_id, int(page + 1))).content
-    oc.add(DirectoryObject(key = Callback(PopulateFromHTML, show_id = id, show_title = title, page = page + 1, replace_parent = replace_parent), title = 'Next Page ...'))
-    dir.Append(Function(DirectoryItem(populateFromHTML, title = 'Next Page ...'),show_id = show_id, show_title = show_title, page = page+1, replaceParent = True))
-  except:
-    pass
+  if len(html.xpath('//li[@class="next"]')) > 0:
+    oc.add(NextPageObject(
+      key = Callback(Episodes, show_url=show_url, show_title=show_title, page=page+1),
+      title = 'Next Page ...'
+    ))
 
   return oc
-
-####################################################################################################
-def GetThumb(url):
-  try:
-    url = url.replace('/4x3/125.jpg', '/16x9/512.jpg')
-    data = HTTP.Request(full_url, cacheTime = CACHE_1WEEK).content
-    return DataObject(data, 'image/jpeg')
-  except:
-    try:
-      data = HTTP.Request(url, cacheTime = CACHE_1WEEK).content
-      return DataObject(data, 'image/jpeg')
-    except:
-      return Redirect(R(ICON))
